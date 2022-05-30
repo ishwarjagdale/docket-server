8e14c337bbe936a726811b305549b0d8c0ca2d75
Ishwar Jagdale <ishwarrules@gmail.com>
2022-05-30 08:56:43 +0530

    initial commit

---
+++ database.js
@@ -0,0 +1,229 @@
+const {Client} = require("pg");
+const jwt = require("jsonwebtoken");
+const {TokenExpiredError} = require("jsonwebtoken");
+
+const credentials = {
+    user: "postgres",
+    password: "ishwar@01",
+    database: "docket",
+    host: "localhost",
+    port: 5432,
+}
+
+const handleError = (err) => {
+    console.log("ERROR: ", err.stack);
+    return [false, err.toString()];
+}
+
+let client = null;
+
+async function initialize() {
+
+    client = new Client(credentials);
+    client.connect();
+
+    return await client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';", (err, res) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+
+            console.log(":: Connecting to database");
+            let tables = res.rows.map((row) => row.table_name);
+            console.log(":: Initializing database");
+            console.log(":: Tables available: ", tables);
+            // CREATING users TABLE
+            if (!tables.includes("users"))
+                client.query(`
+                    CREATE TABLE IF NOT EXISTS users
+                    (
+                        id           BIGSERIAL PRIMARY KEY,
+                        name         VARCHAR     NOT NULL,
+                        email        VARCHAR     NOT NULL UNIQUE,
+                        password     VARCHAR     NOT NULL,
+                        date_created TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+                    );
+                `, (err, res) => {
+                    if (err) {
+                        console.log(err.stack);
+                    } else {
+                        console.log("\tCreated table : 'users'");
+                        return client;
+                    }
+                });
+
+            // CREATING notes TABLE
+            if (!tables.includes("notes"))
+                client.query(`
+                    CREATE TABLE IF NOT EXISTS notes
+                    (
+                        id            BIGSERIAL PRIMARY KEY,
+                        title         VARCHAR,
+                        content       VARCHAR,
+                        category      VARCHAR,
+                        owner         BIGSERIAL   NOT NULL,
+                        color         VARCHAR,
+                        mark          VARCHAR,
+                        encrypted     VARCHAR,
+                        date_created  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        date_modified TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        FOREIGN KEY (owner) REFERENCES users (id)
+                    )
+                `, (err, res) => {
+                    if (err) {
+                        console.log(err.stack);
+                    } else {
+                        console.log("\tCreated table : 'notes'");
+                        return client;
+                    }
+                });
+
+            console.log(":: Initialization Completed");
+
+        }
+    });
+}
+
+const createToken = (id, email) => {
+    let token;
+    try {
+        token = jwt.sign({
+            userId: id, email: email
+        }, process.env.secret_key, {expiresIn: "1d"});
+        return [true, token];
+    } catch (e) {
+        console.log(e);
+        return [false, "JWTError"];
+    }
+}
+
+async function login(email, password) {
+    return await client.query("SELECT * FROM users WHERE email = $1", [email]).then((res) => {
+        if (res.rows.length) {
+            let user = res.rows[0];
+            if (user["password"] === password) {
+                let token = createToken(user['id'], user['email']);
+                if(!token[0]) return token;
+                console.log(`Activity: Login: ${email}`)
+                delete user["password"];
+                return [true, user, token[1]];
+            } else {
+                return [false, "wrong credentials"];
+            }
+        } else {
+            return [false, "user not found"];
+        }
+    }).catch(handleError)
+}
+
+async function register(name, email, password) {
+    return await client.query(`
+        INSERT INTO users (name, email, password)
+        VALUES ($1, $2, $3)
+    `, [name, email, password]).then(async (res) => {
+        const userQuery = await client.query(`
+            SELECT *
+            FROM users
+            WHERE email = $1
+        `, [email])
+        if (userQuery.rows.length) {
+            let user = userQuery.rows[0];
+            let token = createToken(user.id, user.email);
+            if(!token[0]) return token;
+            console.log(`Activity: Registered: ${user.email}`)
+            delete user["password"];
+            return [true, user, token[1]];
+        } else {
+            return [false, "Something went wrong!"];
+        }
+    }).catch(handleError)
+}
+
+function verifyToken(token) {
+    try {
+        return [true, jwt.verify(token, process.env.secret_key)];
+    } catch (TokenExpiredError) {
+        return [false, TokenExpiredError.toString()];
+    }
+}
+
+async function getUser(dToken) {
+    return await client.query("SELECT * FROM users WHERE id = $1", [dToken['userId']]).then((res) => {
+        if (res.rows.length) {
+            let user = res.rows[0];
+            delete user.password;
+            return user;
+        } else {
+            return false;
+        }
+
+    }).catch(handleError);
+}
+
+async function getNotes(userId) {
+    return await client.query(`SELECT id, title, category, owner, color, mark, encrypted, date_created, date_modified
+                               FROM notes
+                               WHERE owner = $1 ORDER BY date_created`, [userId]).then((res) => {
+        return [true, res.rows];
+    }).catch(handleError)
+}
+
+async function getNote(userId, noteId) {
+    return await client.query(`SELECT * FROM notes WHERE id = $1 AND owner = $2`, [noteId, userId]).then((res) => {
+        if(res.rows.length) {
+            return [true, res.rows[0]];
+        } else {
+            return [false, "Note doesn't exist"];
+        }
+    }).catch(handleError)
+}
+
+async function newNote(note) {
+    if(note.hasOwnProperty("id")) {
+        return await client.query(`
+        UPDATE notes SET 
+             title = $1, 
+             content = $2, 
+             category = $3, 
+             owner = $4, 
+             color = $5, 
+             mark = $6, 
+             encrypted = $7
+        WHERE id = $8;
+    `, [note.title, note.content, note.category, note.owner, note.color, note.mark, note.encrypted, note.id]
+        ).then((res) => {
+            return [true, res];
+        }).catch(handleError);
+    } else {
+        return await client.query(`
+        INSERT INTO notes (
+                           title, content, category, owner, color, mark, encrypted
+        ) VALUES (
+                  $1, $2, $3, $4, $5, $6, $7
+                 )
+    `, [note.title, note.content, note.category, note.owner, note.color, note.mark, note.encrypted]
+        ).then((res) => {
+            return [true, res];
+        }).catch(handleError);
+    }
+}
+
+async function deleteNote(userId, noteId) {
+    return await client.query(`
+        DELETE FROM notes WHERE id = $1 AND owner = $2;
+    `, [noteId, userId]).then((res) => {
+        return [true, res];
+    }).catch(handleError);
+}
+
+module.exports = {
+    "initialize": initialize,
+    "login": login,
+    "client": client,
+    "verifyToken": verifyToken,
+    "getUser": getUser,
+    "getNotes": getNotes,
+    "getNote": getNote,
+    "newNote": newNote,
+    "deleteNote": deleteNote,
+    "register": register,
+}
---
+++ index.js
@@ -0,0 +1,167 @@
+const express = require("express");
+const { Pool, Client } = require("pg");
+const { expressjwt: jwt } = require("express-jwt");
+
+const db = require("./database");
+const {verify} = require("jsonwebtoken");
+const {getUser} = require("./database");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+let sessions  = {
+  inSession: [],
+  outOfSession: []
+};
+
+(async () => {
+    await db.initialize().then((res) => {
+        db.client = res;
+    });
+})();
+
+const getCookieToken = (req) => {
+    let jar = {};
+    req.headers.cookie?.split(" ").forEach((cookie) => {
+        cookie = (cookie.endsWith(";") ? cookie.slice(0, cookie.length - 1) : cookie).split("=");
+        jar[cookie[0]] = cookie[1];
+    } )
+    req.userToken = jar['sessionToken'];
+    return jar['sessionToken'];
+};
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hi from server!");
+});
+
+app.post("/login", (req, res) => {
+    if (req.body.hasOwnProperty("name") && req.body.hasOwnProperty("email") && req.body.hasOwnProperty("password")) {
+        db.login(req.body.email, req.body.password).then((r) => {
+            console.log(r);
+            if (r[0]) {
+                sessions.inSession.push(r[2]);
+                console.log(sessions);
+                res.cookie('sessionToken', r[2], {httpOnly: true, maxAge: 86400});
+                res.status(200).json(r[1]);
+            } else {
+                res.status(400).json(r[1]);
+            }
+        })
+    }
+})
+
+
+app.post("/register", (req, res) => {
+    if (req.body.hasOwnProperty("name") && req.body.hasOwnProperty("email") && req.body.hasOwnProperty("password")) {
+        db.register(req.body.name, req.body.email, req.body.password).then((r) => {
+            console.log(r);
+            if (r[0]) {
+                sessions.inSession.push(r[2]);
+                res.cookie('sessionToken', r[2], {httpOnly: true, maxAge: 86400});
+                res.status(200).json(r[1]);
+            } else {
+                res.status(400).json(r[1]);
+            }
+        })
+    }
+})
+
+app.get("/auth/verify", (req, res) => {
+    let dToken = db.verifyToken(getCookieToken(req));
+    if(dToken[0]) {
+        db.getUser(dToken[1]).then((r) => {
+            if(r) {
+                res.status(200).json(r);
+            } else {
+                res.status(401).json("Invalid credentials!");
+            }
+        });
+    } else {
+        res.status(401).json("Authorization required!");
+    }
+})
+
+app.use("/api", jwt({ secret: process.env.secret_key, algorithms: ["HS256"], getToken:  getCookieToken}));
+
+app.get("/api/notes", (req, res) => {
+    let userId = db.verifyToken(req.userToken)[1]['userId'];
+    db.getNotes(userId).then((r) => {
+        if(r[0]) {
+            res.status(200).json(r[1]);
+        } else {
+            res.status(400).json(r[1]);
+        }
+    });
+});
+
+app.get("/api/notes/:noteId", (req, res) => {
+    let userId = db.verifyToken(req.userToken)[1]['userId'];
+    let noteId = req.params.noteId;
+    db.getNote(userId, noteId).then((r) => {
+        if(r[0]) {
+            res.status(200).json(r[1]);
+        } else {
+            res.status(400).json(r[1]);
+        }
+    })
+})
+
+app.delete("/api/notes/:noteId", (req, res) => {
+    let userId = db.verifyToken(req.userToken)[1]['userId'];
+    let noteId = req.params.noteId;
+    db.deleteNote(userId, noteId).then((r) => {
+        if(r[0]) {
+            res.status(200).json(r[1]);
+        } else {
+            res.status(400).json(r[1]);
+        }
+    })
+})
+
+app.post("/api/notes", (req, res) => {
+    let userId = Number.parseInt(db.verifyToken(req.userToken)[1]['userId']);
+    let note = {
+        title: "",
+        content: "",
+        category: "",
+        owner: userId,
+        color: "",
+        mark: null,
+        encrypted: null,
+        ...req.body
+    };
+    if(Number.parseInt(note.owner) === userId) {
+        db.newNote(note).then((r) => {
+            if(r[0]) {
+                res.status(200).json(r[1]);
+            } else {
+                res.status(500).json("Something went wrong");
+            }
+        })
+    } else {
+        res.status(401).json("Unauthorized request");
+    }
+})
+
+app.get("/auth/logout", (req, res) => {
+    let userId = Number.parseInt(db.verifyToken(getCookieToken(req))[1]['userId']);
+    let userToken = req.userToken;
+    sessions.inSession.splice(sessions.inSession.indexOf(userToken), 1);
+    sessions.outOfSession.push(userToken);
+    console.log(sessions);
+    res.cookie('sessionToken', null, {
+        maxAge: -1,
+    })
+    res.status(200).json(true);
+})
+
+app.get("/test", (req, res) => {
+    db.client.query(req.body)
+})
+
+app.listen(PORT, () => {
+    console.log(`docket server listening on ${PORT}`);
+});
